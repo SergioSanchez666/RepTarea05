697056db9e256afbe6c82bcf001baf60d0942a0d
SergioSanchez666 <L20070523@cdmadero.tecnm.mx>
2025-03-18 09:48:02 -0600

    Principales metodos utilizando varargs

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,6 @@
 @file:JvmName("MyApp")
 package my.program
 
-class App {
-    companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            App().run()
-        }
-    }
-    fun run() {
-        println("Hello World!!!")
-    }
+fun main(vararg args: String) {
+    println("Hello, world!!!!")
 }
