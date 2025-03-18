3a039f6c6d08c891f3e7a38bd576674dfaac4afb
SergioSanchez666 <L20070523@cdmadero.tecnm.mx>
2025-03-18 09:44:53 -0600

    Variacion del ejercicio anterior para hacer el Hola mas real

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,10 @@ package my.program
 class App {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
-            println("Hello World!!!")
+            App().run()
         }
+    }
+    fun run() {
+        println("Hello World!!!")
     }
 }
